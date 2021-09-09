2a25e2838e53b3a1621a6090acceec31d6b137d9
rajatparida86 <rajatparida86@gmail.com>
2021-09-09 12:51:01 +0200

    avoid race conditions

--- internal/pkg/location/inMemoryStore.go
+++ internal/pkg/location/inMemoryStore.go
@@ -3,13 +3,13 @@ package location
 import (
 	"fmt"
 	"github.com/rajatparida86/location-history/internal/pkg/config"
-	log "github.com/sirupsen/logrus"
+	"sync"
 	"time"
 )
 
 type InMemoryStore struct {
 	history map[string][]*Location
-	//lock  sync.Mutex
+	lock    sync.Mutex
 }
 
 func NewInMemoryStore(conf *config.Configuration) *InMemoryStore {
@@ -17,8 +17,8 @@ func NewInMemoryStore(conf *config.Configuration) *InMemoryStore {
 
 	// Expire location entries as per TTL
 	go func() {
-		for current := range time.Tick(time.Second) {
-			//history.lock.Lock()
+		for current := range time.Tick(1 * time.Minute) {
+			store.lock.Lock()
 			for orderId, history := range store.history {
 				for i := len(history) - 1; i >= 0; i-- {
 					if current.Unix()-history[i].createdAt > int64(conf.StoreTtl) {
@@ -27,14 +27,14 @@ func NewInMemoryStore(conf *config.Configuration) *InMemoryStore {
 					}
 				}
 			}
-			//history.lock.Unlock()
+			store.lock.Unlock()
 		}
 	}()
 	return store
 }
 
 func (i *InMemoryStore) UpdateLocation(orderId string, location *Location) error {
-	//i.lock.Lock()
+	i.lock.Lock()
 	locationHistory, ok := i.history[orderId]
 	if !ok {
 		locationHistory = make([]*Location, 0)
@@ -42,35 +42,28 @@ func (i *InMemoryStore) UpdateLocation(orderId string, location *Location) error
 	location.createdAt = time.Now().Unix()
 	locationHistory = append(locationHistory, location)
 	i.history[orderId] = locationHistory
-	//i.lock.Unlock()
+	i.lock.Unlock()
 	return nil
 }
 
 func (i *InMemoryStore) GetLocation(orderId string, depth *int) ([]*Location, error) {
-	//i.lock.Lock()
 	locationHistory, ok := i.history[orderId]
 	if !ok {
-		for k, v := range i.history {
-			log.Info("key %s", k)
-			log.Info("val %s", v)
-		}
 		return nil, fmt.Errorf("order with id - %v not found", orderId)
-
 	}
 	if depth == nil {
 		return locationHistory, nil
 	}
-	//i.lock.Unlock()
 	return locationHistory[len(locationHistory)-*depth:], nil
 }
 
 func (i *InMemoryStore) DeleteLocation(orderId string) error {
-	//i.lock.Lock()
+	i.lock.Lock()
 	_, ok := i.history[orderId]
 	if !ok {
 		return fmt.Errorf("order not found")
 	}
 	delete(i.history, orderId)
-	//i.lock.Unlock()
+	i.lock.Unlock()
 	return nil
 }
